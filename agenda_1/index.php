487492c6fcae148b3a90e8d3e8895858be0bb738
OCHOASV <34662161+OCHOASV@users.noreply.github.com>
2025-02-24 19:50:22 +0100

    Connection

---
+++ agenda_1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Agenda</title>
+</head>
+<body>
+	<form action="">
+		Nombre: <input type="text" id="nombre" name="nombre" placeholder="Nombre">
+		<br>
+		Telefono: <input type="text" id="telefono" name="telefono" placeholder="Telefono">
+		<br>
+		Correo: <input type="text" id="correo" name="correo" placeholder="Correo">
+		<br>
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+</html>
