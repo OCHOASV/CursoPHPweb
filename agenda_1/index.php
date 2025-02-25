515b5489770ea53ebd6bc792602643806bf749cb
OCHOASV <34662161+OCHOASV@users.noreply.github.com>
2025-02-25 19:59:13 +0100

    BS

--- agenda_1/index.php
+++ agenda_1/index.php
@@ -1,5 +1,4 @@
 <?php
-	// conexion
 	include 'conn.php';
 ?>
 <!DOCTYPE html>
@@ -8,18 +7,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agenda</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-    <h2>Agregar Contacto</h2>
-    <form action="crear.php" method="POST">
-        <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" placeholder="Nombre" required><br>
-        <label for="telefono">Teléfono:</label>
-        <input type="text" id="telefono" name="telefono" placeholder="Telefono" required><br>
-        <label for="email">Email:</label>
-        <input type="email" id="correo" name="correo" placeholder="Correo" required><br>
-        <button type="submit">Guardar</button>
-    </form>
+    <nav class="navbar navbar-expand-lg bg-primary">
+        <div class="container">
+            <a class="navbar-brand text-white" href="#">
+                OCHOA <i class="fa-brands fa-php fa-xl"></i>
+            </a>
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="menu">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Productos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                </ul>
+                <span>
+                    <button class="btn btn-sm btn-outline-dark"><i class="fa-regular fa-user"></i> Login</button>
+                </span>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-3 col-md-3">
+        <h2>Agregar Contacto</h2>
+        <form action="crear.php" method="POST">
+            <label class="form-label" for="nombre">Nombre:</label>
+            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre" required><br>
+            <label for="telefono">Teléfono:</label>
+            <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Telefono" required><br>
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="correo" name="correo" placeholder="Correo" required><br>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </form>
+    </div>
 
     <?php
 
@@ -30,14 +65,17 @@
 
         if ($result->num_rows > 0) {
             // Change Table!
-            echo "<table border='1'>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nombre</th>
-                        <th>Teléfono</th>
-                        <th>Email</th>
-                        <th>Acciones</th>
-                    </tr>";
+            echo " <div class='container mt-3'>
+                <table class='table table-hover text-center'>
+                    <thead class='table-dark'>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Teléfono</th>
+                            <th>Email</th>
+                            <th>Acciones</th>
+                        </tr>
+                    <thead>";
             while ($row = $result->fetch_assoc()) {
                 echo "<tr>
                         <td>{$row['id']}</td>
@@ -50,11 +88,22 @@
                         </td>
                     </tr>";
             }
-            echo "</table>";
+            echo "</table>
+
+                <div>";
         } else {
             echo "No hay personas registradas.";
         }
         $conn->close();
     ?>
+
+    <!-- <footer class="text-center text-white mt-5">
+        <div class="bg-primary p-3">
+            Copyright - Carlos Ochoa 2025 -
+            <a class="text-white" href="#">https://ochoa.es</a>
+        </div>
+    </footer> -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
